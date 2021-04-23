e02a719e50d65055c560c7910da7cbc89362e113
Domenic Scarcella <ls41513@yahoo.com>
2021-04-23 19:08:01 -0400

    Copying the week's projects and typing aimlessly (2)

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -12,13 +12,13 @@ function findBy(filter) {
   return db('users').where(filter);
 }
 
-function validateUser(req, res, next) {
+function validateInputsExist(req, res, next) {
   if (!req.body.username || !req.body.username.trim() || !req.body.password || !req.body.password.trim()) {
     next({ status: 401, message: 'username and password required' });
   } else {
     next();
   }
-};
+}
 
 async function uniqueUsername(req, res, next) {
   try {
@@ -31,7 +31,21 @@ async function uniqueUsername(req, res, next) {
   } catch (err) {
     next(err);
   }
-};
+}
+
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (users.length) {
+      req.user = users[0];
+      next();
+    } else {
+      next({ status: 401, message: "invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
+}
 
 async function addUser({ username, password }) {
   let created_id;
@@ -44,33 +58,32 @@ async function addUser({ username, password }) {
 
 // Router functions here
 
-router.post('/register', validateUser, uniqueUsername, (req, res, next) => {
-  res.end('implement register, please!');
+router.post('/register', validateInputsExist, uniqueUsername, (req, res, next) => {
   /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+   IMPLEMENT
+   You are welcome to build additional middlewares to help with the endpoint's functionality.
+   DO NOT EXCEED 2^8 ROUNDS OF HASHING!
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
+   1- In order to register a new account the client must provide `username` and `password`:
+     {
+       "username": "Captain Marvel", // must not exist already in the `users` table
+       "password": "foobar"          // needs to be hashed before it's saved
+     }
 
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
+   2- On SUCCESSFUL registration,
+     the response body should have `id`, `username` and `password`:
+     {
+       "id": 1,
+       "username": "Captain Marvel",
+       "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
+     }
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+   3- On FAILED registration due to `username` or `password` missing from the request body,
+     the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+   4- On FAILED registration due to the `username` being taken,
+     the response body should include a string exactly as follows: "username taken".
+ */
   const { username, password } = req.body;
   const hash = bcrypt.hashSync(password, 8);
 
@@ -81,31 +94,37 @@ router.post('/register', validateUser, uniqueUsername, (req, res, next) => {
     .catch(next);
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', validateInputsExist, checkUsernameExists, (req, res, next) => {
   /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
+  IMPLEMENT
+  You are welcome to build additional middlewares to help with the endpoint's functionality.
 
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
+  1- In order to log into an existing account the client must provide `username` and `password`:
+    {
+      "username": "Captain Marvel",
+      "password": "foobar"
+    }
 
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
+  2- On SUCCESSFUL login,
+    the response body should have `message` and `token`:
+    {
+      "message": "welcome, Captain Marvel",
+      "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
+    }
 
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
+  3- On FAILED login due to `username` or `password` missing from the request body,
+    the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+  4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
+    the response body should include a string exactly as follows: "invalid credentials".
+*/
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.json({ message: `Welcome ${req.user.username}`, token });
+  } else {
+    next({ status: 401, message: 'invalid credentials' });
+  }
 });
 
 module.exports = router;
