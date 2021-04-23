3e5e165a9e61602e747f8bbd86cb27f926f1226b
Domenic Scarcella <ls41513@yahoo.com>
2021-04-23 13:18:31 -0400

    More dependencies and added folders/files

--- api/server.js
+++ api/server.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
+const knex = require('../data/dbConfig.js');
 
 const restrict = require('./middleware/restricted.js');
-
 const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
 
 const server = express();
+
+server.use(session({
+    name: 'chocolatechip',
+    secret: 'shh',
+    saveUninitialized: false,
+    resave: false,
+    store: new Store({
+        knex,
+        createtable: true,
+        clearInterval: 1000 * 60 * 10,
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+    }),
+    cookie: {
+        maxAge: 1000 * 60 * 30,
+        secure: false,
+        httpOnly: true,
+    }
+}));
 
 server.use(helmet());
 server.use(cors());
@@ -15,5 +36,12 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
 
 module.exports = server;
